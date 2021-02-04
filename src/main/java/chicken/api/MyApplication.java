345f1ddd963310d30e1571117987c0bbe55fe723
Sriman <Devopssriman1@gmail.com>
2021-02-04 22:22:25 +0530

    code zonoto biryani

---
+++ src/main/java/chicken/api/MyApplication.java
@@ -0,0 +1,12 @@
+package chicken.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Application class.
+ */
+@ApplicationPath("")
+public class MyApplication extends Application {
+
+}
